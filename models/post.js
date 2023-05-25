cb5cd257df5cbe96b92dc9ea4a774972226f2df9
Benjamin Yong <benj_yongzh@hotmail.com>
2023-05-25 10:57:51 +0800

    Post text is no longer required.

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   title: { type: String, required: true },
-  text: { type: String, required: true },
+  text: { type: String },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   date_of_post: { type: Date, required: true, default: Date.now },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
