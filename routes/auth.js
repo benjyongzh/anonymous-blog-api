62442c9f6405d835c481ab56e2564949ad60bc3e
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-05 11:48:18 +0800

    Change res.send to return res.json.

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/user_controller");
 
 /* GET login page. */
 router.get("/login", (req, res) => {
-  res.send(200, "Login page");
+  return res.status(200).json("Login page");
 });
 
 /* POST login page. */
@@ -13,7 +13,7 @@ router.post("/login", userController.user_login_post);
 
 /* GET signup page. */
 router.get("/signup", (req, res) => {
-  res.send(200, "sign up page");
+  return res.status(200).json("Sign up page");
 });
 
 /* POST signup page. */
@@ -31,7 +31,7 @@ router.get("/loggingout", (req, res, next) => {
 
 /* GET logged out page. */
 router.get("/logout", (req, res, next) => {
-  res.send(200, "logout page");
+  return res.status(200).json("Logout page");
 });
 
 module.exports = router;
