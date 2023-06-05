8c56ee6bba9e822ea009ce5e69a224bf427e4bf3
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-05 16:39:51 +0800

    Create optional verifytoken for pages where logging in is optional.

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -3,15 +3,21 @@ const Post = require("../models/post");
 
 const asyncHandler = require("express-async-handler");
 
-//GET main page, sort posts according to date of posting
-exports.main_page_get = asyncHandler(async (req, res, next) => {
-  const allPosts = await Post.find({})
-    .populate({ path: "user", options: { retainNullValues: true } })
-    .sort({ date_of_post: 1 })
-    .exec();
+//custom middlware
+const { verifyTokenOptional } = require("../middleware/jwtVerification");
 
-  return res.json({
-    user: req.user,
-    posts: allPosts,
-  });
-});
+//GET main page, sort posts according to date of posting
+exports.main_page_get = [
+  verifyTokenOptional,
+  asyncHandler(async (req, res, next) => {
+    const allPosts = await Post.find({})
+      .populate({ path: "user", options: { retainNullValues: true } })
+      .sort({ date_of_post: 1 })
+      .exec();
+
+    return res.json({
+      user: req.user,
+      posts: allPosts,
+    });
+  }),
+];
--- middleware/jwtVerification.js
+++ middleware/jwtVerification.js
@@ -2,7 +2,12 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-function verifyToken(req, res, next) {
+function tokenIsOptional(req, res, next) {
+  req.body.tokenOptional = true;
+  next();
+}
+
+function checkToken(req, res, next) {
   //get auth header value
   const bearerHeader = req.headers["authorization"];
   //format of token:
@@ -11,17 +16,26 @@ function verifyToken(req, res, next) {
   //check if bearer is undefined
   //split at space
   const bearerToken = bearerHeader && bearerHeader.split(" ")[1];
-  //forbidden
-  if (bearerToken == null) return res.status(401).json("Must log in first");
-
-  //valid token
-  jwt.verify(bearerToken, process.env.JWT_SECRET_KEY, (err, authData) => {
-    if (err) {
-      res.status(403).json(err);
+  //check if logged in or forbidden
+  if (bearerToken == null) {
+    if (req.body.tokenOptional) {
+      next();
+    } else {
+      return res.status(401).json("Must log in first");
     }
-    req.user = authData.user;
-    next();
-  });
+  } else {
+    //valid token
+    jwt.verify(bearerToken, process.env.JWT_SECRET_KEY, (err, authData) => {
+      if (err) {
+        res.status(403).json(err);
+      }
+      req.user = authData.user;
+      next();
+    });
+  }
 }
 
-module.exports = { verifyToken };
+const verifyToken = [checkToken];
+const verifyTokenOptional = [tokenIsOptional, checkToken];
+
+module.exports = { verifyToken, verifyTokenOptional };
