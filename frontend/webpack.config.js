c7156473c794c3a7e48afc204c1e7bcd7db5f5dc
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-08 15:13:05 +0800

    Redo CRA as manually installed packages from webpack and babel.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,22 +1,52 @@
-// const Dotenv = require("dotenv-webpack");
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 // module.exports = {
-//   plugins: [
-//     new Dotenv({
-//       path: `./.env`,
-//     }),
-//   ],
+//   mode: "development",
+// plugins: [
+//   new HtmlWebpackPlugin({
+//     title: 'Development',
+//   }),
+// ],
 // };
 
-// const webpack = require("webpack"); // only add this if you don't have yet
-
-// // replace accordingly './.env' with the path of your .env file
-// require("dotenv").config({ path: "./.env" });
-
-// module.exports = {
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       "process.env": JSON.stringify(process.env),
-//     }),
-//   ],
-// };
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      /*Choose only one of the following two: if you're using 
+      plain CSS, use the first one, and if you're using a
+      preprocessor, in this case SASS, use the second one*/
+      // {
+      //   test: /\.css$/,
+      //   use: ["style-loader", "css-loader"],
+      // },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
