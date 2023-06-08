dc1158b85074abc0f34340a8be27eb4582369586
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-08 15:18:11 +0800

    Adjust package scripts to suit dev vs prod.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,17 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-// module.exports = {
-//   mode: "development",
-// plugins: [
-//   new HtmlWebpackPlugin({
-//     title: 'Development',
-//   }),
-// ],
-// };
-
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
   output: {
