5777f27f01f6bf7ddc65755c33d5139990be6b0f
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-14 21:14:40 +0800

    Try different header for Axios.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,16 +3,23 @@ import store from "../Context/store.js";
 
 export default axios.create({
   baseURL: `${process.env.REACT_APP_API_INDEX_URL}${process.env.REACT_APP_BACKEND_PORT}`,
+  // headers: {
+  //   common:
+  //     store.getState().auth.authToken === ""
+  //       ? null
+  //       : {
+  //           //Authorization: localStorage.getItem("auth_token")
+  //           //use redux store for this:
+
+  //           Authorization: "Bearer " + store.getState().auth.authToken,
+  //         },
+  //   post: { "Content-Type": "application/json" },
+  // },
   headers: {
-    common:
+    Authorization:
       store.getState().auth.authToken === ""
         ? null
-        : {
-            //Authorization: localStorage.getItem("auth_token")
-            //use redux store for this:
-
-            Authorization: "Bearer " + store.getState().auth.authToken,
-          },
-    post: { "Content-Type": "application/json" },
+        : `Bearer ${store.getState().auth.authToken}`,
+    "Content-Type": "application/json",
   },
 });
