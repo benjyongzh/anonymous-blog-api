d04272eb7844f87507d203b651a3ab39277c1b1c
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-14 10:52:45 +0800

    Add redux-logger middleware.

--- frontend/src/Context/store.js
+++ frontend/src/Context/store.js
@@ -1,22 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 //middleware
-// const reduxLogger = require("redux-logger");
+import { createLogger } from "redux-logger";
+const logger = createLogger();
 
 //reducers
-import userReducer from "../features/user/userSlice";
-
-// const logger = reduxLogger.createLogger();
+import authReducer from "../Features/auth/authSlice";
 
 //configureStore takes an object as an arg
 //this object
 const store = configureStore({
   //the reducer key is similar to the combineReducer of the original redux
   reducer: {
-    user: userReducer,
+    auth: authReducer,
   },
   //middleware takes a function that has getDefaultMiddleware. concat your middleware to it
-  //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
