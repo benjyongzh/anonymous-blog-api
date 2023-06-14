5bddbed7da7e1f537a5ccb675428687d68c8b76f
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-14 10:55:06 +0800

    Move redux files.

--- frontend/src/Features/auth/UserView.js
+++ frontend/src/Features/auth/UserView.js
@@ -1,3 +1,32 @@
+/* import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./authSlice";
+
+export const UserView = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchData("/"));
+  }, []);
+
+  return (
+    <div>
+      <h2>List of users</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+ */
+
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ordered, restocked } from "./cakeSlice";
