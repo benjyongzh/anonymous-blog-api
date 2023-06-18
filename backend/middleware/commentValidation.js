4c525aab1258f434dcd3fc8ea90941146311bfad
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-18 15:54:31 +0800

    Add minimum characters for comment and reply.

--- backend/middleware/commentValidation.js
+++ backend/middleware/commentValidation.js
@@ -4,16 +4,16 @@ const commentValidation = [
   //validation and sanitization of fields
   body("new_comment")
     .trim()
-    .isLength({ max: 300 })
-    .withMessage("Comment must be maximum of 300 characters"),
+    .isLength({ min: 1, max: 300 })
+    .withMessage("Comment must be between 1 to 300 characters"),
 ];
 
 const replyValidation = [
   //validation and sanitization of fields
   body("new_reply")
     .trim()
-    .isLength({ max: 100 })
-    .withMessage("Reply must be maximum of 100 characters"),
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Reply must be between 1 to 100 characters"),
 ];
 
 module.exports = { commentValidation, replyValidation };
