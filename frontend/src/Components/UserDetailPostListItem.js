24815dfb6a94303e3455767abf0701b8a7b82e32
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-20 16:05:03 +0800

    Styling Improvement.

--- frontend/src/Components/UserDetailPostListItem.js
+++ frontend/src/Components/UserDetailPostListItem.js
@@ -8,10 +8,10 @@ function UserDetailPostListItem(props) {
       to={currentPost.url}
       className="list-group-item list-group-item-action"
     >
-      <p>
+      <p className="fst-italic text-secondary">
         {currentPost.date_of_post_formatted} ({currentPost.date_of_post_ago})
       </p>
-      <p className="h5">{currentPost.title}</p>
+      <h5>{currentPost.title}</h5>
       {currentPost.comments.length === 1 ? (
         <p className="mb-0">{currentPost.comments.length} Comment</p>
       ) : (
