ed50cbcf2db80472a4b0df7198bdcaca1488467f
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-22 11:05:20 +0800

    Refactor ignore object of isAlphanumeric.

--- middleware/nameValidation.js
+++ middleware/nameValidation.js
@@ -6,13 +6,13 @@ const nameValidation = [
     .trim()
     .isLength({ min: 1, max: 30 })
     .withMessage("First name must be 1 to 30 characters long")
-    .isAlpha("en-US", { ignore: /\s|\-/g })
+    .isAlpha("en-US", { ignore: " -" })
     .withMessage("First name can only contain alphabets, spaces and hypens."),
   body("last_name", "Last name must not be empty")
     .trim()
     .isLength({ min: 1, max: 30 })
     .withMessage("Last name must be 1 to 30 characters long")
-    .isAlpha("en-US", { ignore: /\s|\-/g })
+    .isAlpha("en-US", { ignore: " -" })
     .withMessage("Last name can only contain alphabets, spaces and hypens."),
 ];
 
